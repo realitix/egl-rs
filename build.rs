27def28339af1fe5076f3d8a182cff8a11ea89c2
Bevilacqua Jean-Sebastien <realitix@gmail.com>
2018-06-20 09:49:53 +0200

    Update build script

--- build.rs
+++ build.rs
@@ -4,10 +4,14 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    let current_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let clang_arg = ["-I", &current_dir, "/include"].join("");
+
     println!("cargo:rustc-link-lib=EGL");
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
+        .clang_arg(clang_arg)
         .generate()
         .expect("Unable to generate bindings");
 
